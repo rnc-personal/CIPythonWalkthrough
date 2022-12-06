4bdfac008ecc856febb21f9372bc92117980be6f
rnc-personal <richard.challenger@protonmail.com>
2022-12-06 20:19:36 +0000

    refactored worksheet update func

--- run.py
+++ run.py
@@ -52,15 +52,14 @@ def validate_data(values):
 
     return True
 
-def update_sales_worksheet(data):
-    """
-    Updates the sales worksheet in GDrive. Adds new row with the proided data
-    """
-    print("Updating sales sheet...\n")
-    sales_worksheet = SHEET.worksheet("sales")
-    sales_worksheet.append_row(data)
-    print("sales worksheet updated succesfully...\n")
+def update_worksheet(data, worksheet):
+    """ New function """
+    print(f"Updating {worksheet}...\n")
 
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} has been updated")
+    
 def calculate_surplus_data(sales_row):
     """
     Works out stock shortages / extra food made for customers
@@ -76,24 +75,15 @@ def calculate_surplus_data(sales_row):
     
     return new_surplus_data
 
-def update_surplus_worksheet(surplus_data):
-    """
-    Updates the surplus worksheet in GDrive. Adds new row with the proided data
-    """
-    print("Updating surplus sheet...\n")
-    surplus_worksheet = SHEET.worksheet("surplus")
-    surplus_worksheet.append_row(surplus_data)
-    print("surplus worksheet updated succesfully...\n")
-
 def main():
     """
     Run all program functions
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data)
+    update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
-    update_surplus_worksheet(new_surplus_data)
+    update_worksheet(new_surplus_data,"surplus")
 
 
 print("Welcome to Love sandiwches automation tool")
