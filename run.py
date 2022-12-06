e8e8e986c9958961b4dcfb7c53bed5cbfc8bc0ac
rnc-personal <richard.challenger@protonmail.com>
2022-12-06 21:00:03 +0000

    dependancies setup for heroku

--- run.py
+++ run.py
@@ -3,7 +3,7 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
+
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -26,7 +26,7 @@ def get_sales_data():
         print("Should be 6 numbers")
         print("e.g 10,20,30,40,50,60\n")
 
-        data_str = input("Enter your data here: ")
+        data_str = input("Enter your data here:\n")
 
         sales_data = data_str.split(",")
 
